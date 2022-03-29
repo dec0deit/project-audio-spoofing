54cba601e90a6223876c7149f62e475e42b12d4b
Nipun <nipun.gupta@krishi.network>
2022-03-29 14:19:29 +0530

    added code

--- frontend/mqcc.m
+++ frontend/mqcc.m
@@ -1,19 +1,19 @@
 clc;
-ap= 'C:\Users\singl\Downloads\Audio-Classification\Replay-Recordings\training\1PR\';
+ap= 'C:\Users\singl\Downloads\Audio-Classification\Replay-Recordings\testing\2PR\';
 audio_files=dir(fullfile(ap,'*.wav'));
 nfiles=length(audio_files);
 disp(nfiles);
-for i=1:3000    
+for i=1:nfiles  
   filename=audio_files(i).name;
-  filename1=strcat('C:\Users\singl\Downloads\Audio-Classification\Replay-Recordings\training\1PR\',filename);
+  filename1=strcat('C:\Users\singl\Downloads\Audio-Classification\Replay-Recordings\testing\2PR\',filename);
   [x,fs]=audioread(filename1);
   [coeffs,delta,deltaDelta,loc]=mfcc(x,fs);
-  disp(coeffs+" "+deltaDelta)
+  %disp(coeffs+" "+deltaDelta)
   F= horzcat(coeffs,delta,deltaDelta);
   M2 = mean(F);
-  M1= 0;
+  M1= 2;
   M= horzcat(M2,M1);
-  dlmwrite('M_TRAIN.csv',M,'delimiter',',','-append');
+  dlmwrite('M_TEST.csv',M,'delimiter',',','-append');
   i = i+1;
 end
 disp("all done")
