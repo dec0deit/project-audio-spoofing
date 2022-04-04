23a85bc55f69eb01cee8cf072e5de1ffeb6a0507
Nipun <nipun.gupta@krishi.network>
2022-04-04 12:20:20 +0530

    added code

--- frontend/gmcc.m
+++ frontend/gmcc.m
@@ -1,33 +1,12 @@
 clc;
-ap= 'C:\Users\singl\Downloads\Audio-Classification\Replay-Recordings\testing\0PR\';
-audio_files=dir(fullfile(ap,'*.wav'));
+ap= 'E:\Eval2019\bonafide\';
+audio_files=dir(fullfile(ap,'*.flac'));
 nfiles=length(audio_files);
 disp(nfiles);
 for i=1:nfiles  
   try
       filename=audio_files(i).name;
-      filename1=strcat('C:\Users\singl\Downloads\Audio-Classification\Replay-Recordings\testing\0PR\',filename);
-      [x,fs]=audioread(filename1);
-      [coeffs,delta,deltaDelta]=gtcc(x,fs);
-      %disp(coeffs+" "+deltaDelta)
-      F= horzcat(coeffs,delta,deltaDelta);
-      M2 = mean(F);
-      M1= 0;
-      M= horzcat(M2,M1);
-      dlmwrite('G_TEST.csv',M,'delimiter',',','-append');
-  catch
-      disp("some error occured in some file")
-  end
-  i=i+1
-end
-ap= 'C:\Users\singl\Downloads\Audio-Classification\Replay-Recordings\testing\1PR\';
-audio_files=dir(fullfile(ap,'*.wav'))
-nfiles=length(audio_files);
-disp(nfiles);
-for i=1:nfiles  
-   try
-      filename=audio_files(i).name;
-      filename1=strcat('C:\Users\singl\Downloads\Audio-Classification\Replay-Recordings\testing\1PR\',filename);
+      filename1=strcat('E:\Eval2019\bonafide\',filename);
       [x,fs]=audioread(filename1);
       [coeffs,delta,deltaDelta]=gtcc(x,fs);
       %disp(coeffs+" "+deltaDelta)
@@ -35,28 +14,7 @@ for i=1:nfiles
       M2 = mean(F);
       M1= 1;
       M= horzcat(M2,M1);
-      dlmwrite('G_TEST.csv',M,'delimiter',',','-append');
-  catch
-      disp("some error occured in some file")
-  end
-  i=i+1
-end
-ap= 'C:\Users\singl\Downloads\Audio-Classification\Replay-Recordings\testing\2PR\';
-audio_files=dir(fullfile(ap,'*.wav'));
-nfiles=length(audio_files);
-disp(nfiles);
-for i=1:nfiles  
-   try
-      filename=audio_files(i).name;
-      filename1=strcat('C:\Users\singl\Downloads\Audio-Classification\Replay-Recordings\testing\2PR\',filename);
-      [x,fs]=audioread(filename1);
-      [coeffs,delta,deltaDelta]=gtcc(x,fs);
-      %disp(coeffs+" "+deltaDelta)
-      F= horzcat(coeffs,delta,deltaDelta);
-      M2 = mean(F);
-      M1= 2;
-      M= horzcat(M2,M1);
-      dlmwrite('G_TEST.csv',M,'delimiter',',','-append');
+      dlmwrite('G_TEST_LA19.csv',M,'delimiter',',','-append');
   catch
       disp("some error occured in some file")
   end
--- frontend/mqcc.m
+++ frontend/mqcc.m
@@ -1,19 +1,19 @@
 clc;
-ap= 'C:\Users\singl\Downloads\Audio-Classification\Replay-Recordings\testing\2PR\';
+ap= 'E:\LA_TRAIN_19\bonafide\';
 audio_files=dir(fullfile(ap,'*.wav'));
 nfiles=length(audio_files);
 disp(nfiles);
 for i=1:nfiles  
   filename=audio_files(i).name;
-  filename1=strcat('C:\Users\singl\Downloads\Audio-Classification\Replay-Recordings\testing\2PR\',filename);
+  filename1=strcat('E:\LA_TRAIN_19\bonafide\',filename);
   [x,fs]=audioread(filename1);
   [coeffs,delta,deltaDelta,loc]=gtcc(x,fs);
   %disp(coeffs+" "+deltaDelta)
   F= horzcat(coeffs,delta,deltaDelta);
   M2 = mean(F);
-  M1= 2;
+  M1= 1;
   M= horzcat(M2,M1);
-  dlmwrite('G_TEST.csv',M,'delimiter',',','-append');
+  dlmwrite('M_TRAIN_LA19.csv',M,'delimiter',',','-append');
   i = i+1;
 end
 disp("all done")
