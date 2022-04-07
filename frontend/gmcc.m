3388020d2726d5229f08190ae9f0e9dd6441a924
Nipun <nipun.gupta@krishi.network>
2022-04-07 23:43:22 +0530

    added code:

--- frontend/gmcc.m
+++ frontend/gmcc.m
@@ -1,20 +1,19 @@
 clc;
-ap= 'E:\Eval2019\spoof\';
+ap= 'C:\LA\LA\ASVspoof2019_LA_eval\flac';
 audio_files=dir(fullfile(ap,'*.flac'));
 nfiles=length(audio_files);
 disp(nfiles);
-for i=1:nfiles  
+for i=1:20000  
   try
       filename=audio_files(i).name;
-      filename1=strcat('E:\Eval2019\spoof\',filename);
+      disp(filename)
+      filename1=strcat('C:\LA\LA\ASVspoof2019_LA_eval\flac\',filename);
       [x,fs]=audioread(filename1);
       [coeffs,delta,deltaDelta]=gtcc(x,fs);
       %disp(coeffs+" "+deltaDelta)
       F= horzcat(coeffs,delta,deltaDelta);
       M2 = mean(F);
-      M1= 0;
-      M= horzcat(M2,M1);
-      dlmwrite('G_TEST_LA19.csv',M,'delimiter',',','-append');
+      dlmwrite('G_TEST_LA19_BIG_DATA.csv',M2,'delimiter',',','-append');
   catch
       disp("some error occured in some file")
   end
